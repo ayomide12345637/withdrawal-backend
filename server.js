073399b595ff9c74fd5fb5f62869c7b7eeb4da0e
ayomide12345637 <anjorin5888@gmail.com>
2025-11-18 17:55:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const SECRET_KEY = process.env.SQUAD_SECRET_KEY;
